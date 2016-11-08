58c676ee25805dcf206f1688e8467e550f52fb0f
Nathan Venn <natevenn@gmail.com>
2016-11-08 08:33:52 -0700

    refactor and restructure

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,11 @@ const pg = require('pg');
 const path = require('path');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '..', '..', 'client', 'views', 'index.html' ));
 });
 
 /* GET items */
